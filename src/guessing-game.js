d2d46a15b61e92a6e174fdd6f6c831f684f53290
Dasha Konchyts <forexamplednk@gmail.com>
2022-08-14 23:06:05 +0300

    solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.low = min;
+      this.high = max;
     }
-
+  
     guess() {
-
+      this.mid = Math.ceil((this.low + this.high) / 2);
+      return this.mid;
     }
-
+  
     lower() {
-
+      this.high = this.mid;
     }
-
+  
     greater() {
-
+      this.low = this.mid;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
